1cf7fe0251f4510d2cd6c202af4f9f553af407c9
Pochyxi <adienerlopez@gmail.com>
2023-02-12 04:43:37 +0100

    Initial commit

---
+++ src/main/java/com/adienerDB/adienerDB/AdienerDbApplication.java
@@ -0,0 +1,13 @@
+package com.adienerDB.adienerDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdienerDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdienerDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adienerDB/adienerDB/AdienerDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adienerDB.adienerDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdienerDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
