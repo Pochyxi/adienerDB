db23e3daa3121344706cef3e5dd389079f6b7e7e
Pochyxi <adienerlopez@gmail.com>
2023-02-12 13:00:40 +0100

    updt

--- src/main/java/com/adienerDB/adienerDB/AdienerDbApplication.java
+++ src/main/java/com/adienerDB/adienerDB/AdienerDbApplication.java
@@ -2,12 +2,34 @@ package com.adienerDB.adienerDB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class AdienerDbApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AdienerDbApplication.class, args);
+
+		@RequestMapping("/")
+		@RestController
+		class SayHello {
+
+
+			@GetMapping("")
+			public List<String> myName() {
+				List<String> miaLista =  new ArrayList<>();
+				miaLista.add( "Adiener" );
+				miaLista.add( "Lopez" );
+				miaLista.add( "Velazquez" );
+				return miaLista;
+			}
+		}
 	}
 
 }
